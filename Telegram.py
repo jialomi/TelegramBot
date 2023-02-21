4bbd34d016d4479ab04f18b14c9195e59f7502c2
jialomi <73451570+jialomi@users.noreply.github.com>
2023-02-22 02:25:47 +0800

    Add files via upload

---
+++ Telegram.py
@@ -0,0 +1,13 @@
+import telebot
+
+# Replace with your own Telegram bot token
+bot = telebot.TeleBot("1793841737:AAFFzzD2LkBuW1kg0aNq7dKLYNHuAy1BHP4")
+
+# Replace with your own Telegram chat ID
+chat_id = "301795147"
+
+@bot.message_handler(commands=['start'])
+def start_message(msg):
+    bot.send_message(chat_id, 'Test bot for heroku started up')
+
+bot.polling()
