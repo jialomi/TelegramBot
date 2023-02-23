f7c7ea63e46e5d496d4b9e536233afc8dd7e469e
jialomi <73451570+jialomi@users.noreply.github.com>
2023-02-23 18:12:36 +0800

    count command

--- Telegram.py
+++ Telegram.py
@@ -1,4 +1,5 @@
 import telebot
+import re
 
 # Replace with your own Telegram bot token
 bot = telebot.TeleBot("1793841737:AAFFzzD2LkBuW1kg0aNq7dKLYNHuAy1BHP4")
@@ -14,6 +15,13 @@ def start_message(msg):
 @bot.message_handler(commands=['help'])
 def help_message(msg):
     bot.send_message(chat_id, 'second test for raspberry pi hosted bot lel')
+    
+@bot.message_handler(commands=['count'])
+def count_message(msg):
+    param = re.search(r'/count (\d+)', msg.txt)
+    if param:
+        value = param.group(1)
+        bot.send_message(chat_id, f'You have entered {value}')
 
 # Start the bot
 bot.polling()
